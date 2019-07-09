1692ff198dac93cc9522b87e0e86a7d130bf5e68
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-07-09 16:32:34 +0200

    Use the JavaScript transpilation logic

--- worker.js
+++ worker.js
@@ -1,10 +1,26 @@
-self.addEventListener('install', event => {
-  console.log('install');
+self.importScripts('https://unpkg.com/typescript/lib@3.5.3/typescript.js');
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(intercept(event.request));
 });
 
-self.addEventListener('fetch', async event => {
-  console.log('fetch', event.request.url);
-  if (event.request.url.endsWith('.ts')) {
-    event.respondWith(Promise.resolve(new Response(event.request.url), { headers: { 'Content-Type': 'application/javascript' }, status: 200 }));
+async function intercept(request) {
+  const response = await fetch(request);
+  if (!request.url.endsWith('.ts') || !response.ok) {
+    return response;
   }
-});
+  
+  try {
+    return new Response(ts.transpileModule(await response.text(), { compilerOptions: { target: 'esnext' } }).outputText, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: {
+        ...response.headers,
+        // Make the browser know this is now a JavaScript file
+        'Content-Type': 'text/javascript',
+      }
+    });
+  } catch (error) {
+    return response;
+  }
+}
