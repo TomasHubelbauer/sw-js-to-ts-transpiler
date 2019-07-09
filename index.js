911897b47edaee108e5aa0d6a139c7500ae44341
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-07-09 16:24:45 +0200

    Add logs for the install and fetch events

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('test');
+self.addEventListener('install', event => {
+  console.log('install');
+});
+
+self.addEventListener('fetch', async event => {
+  console.log('fetch', event.request.url);
+});
